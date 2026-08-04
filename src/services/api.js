a8ea289a46bcc0b17742d3fdb96b8b3a601f05e2
Farid <geelgaga@gmail.com>
2026-08-04 01:12:40 -0400

    Fix SyntaxError in api.js due to PowerShell formatting

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-﻿import axios from 'axios';
+import axios from 'axios';
 import useAuthStore from '../store/authStore';
 
 console.log('URL QUE ESTOY USANDO:', import.meta.env.VITE_API_URL || 'http://localhost:3001/api');
@@ -10,7 +10,7 @@ const api = axios.create({
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
-    config.headers.Authorization = \Bearer \\;
+    config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
